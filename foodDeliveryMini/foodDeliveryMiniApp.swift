d102e3d40607e6f305fc0b8235793a402d53c65b
krishpatel2404 <krishpatel2404@gmail.com>
2025-02-03 04:45:52 -0800

    Initial Commit

---
+++ foodDeliveryMini/foodDeliveryMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodDeliveryMiniApp.swift
+//  foodDeliveryMini
+//
+//  Created by Teacher on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct foodDeliveryMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
